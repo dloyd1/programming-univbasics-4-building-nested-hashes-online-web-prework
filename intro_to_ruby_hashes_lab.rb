79d8b576058f4458c9d729c653ba39397aec6968
Daniel Loyd <dhloyd@icloud.com>
2020-02-02 11:00:21 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -26,33 +26,13 @@ def monopoly_with_third_tier
 	      reading_railroad: {},
 	      pennsylvania_railroad: {},
 	      b_and_o_railroad: {},
-	      
+	      shortline_railroad: {}
 	    }
 	   }
-	  } 
-	} 
+	 } 
 end
 
 def monopoly_with_fourth_tier
   
 end
 
- describe "the Hashed pointed to by :names" do
-        it "has a key :reading_railroad that points to an empty Hash" do
-          expect(monopoly_with_third_tier[:railroads][:names].has_key?(:reading_railroad)).to(be_truthy, "key :reading_railroad should be present in this Hash")
-          expect(monopoly_with_third_tier[:railroads][:names][:reading_railroad]).to(eq({}), ":reading_railroad should point to an empty Hash")
-        end
-
-        it "has a key :pennsylvania_railroad that points to an empty Hash" do
-          expect(monopoly_with_third_tier[:railroads][:names].has_key?(:pennsylvania_railroad)).to(be_truthy, "key :pennsylvania_railroad should be present in this Hash")
-          expect(monopoly_with_third_tier[:railroads][:names][:pennsylvania_railroad]).to(eq({}), ":pennsylvania_railroad should point to an empty Hash")
-        end
-
-        it "has a key :b_and_o_railroad that points to an empty Hash" do
-          expect(monopoly_with_third_tier[:railroads][:names].has_key?(:b_and_o_railroad)).to(be_truthy, "key :b_and_o_railroad should be present in this Hash")
-          expect(monopoly_with_third_tier[:railroads][:names][:b_and_o_railroad]).to(eq({}), ":b_and_o_railroad should point to an empty Hash")
-        end
-
-        it "has a key :shortline_railroad that points to an empty Hash" do
-          expect(monopoly_with_third_tier[:railroads][:names].has_key?(:shortline_railroad)).to(be_truthy, "key :shortline_railroad should be present in this Hash")
-          expect(monopoly_with_third_tier[:railroads][:names][:shortline_railroad]).to(eq({}), ":shortline_railroad should point to an empty Hash")
