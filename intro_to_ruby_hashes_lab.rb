88c4f512efc5cc9587977a04bf1f76d07eec5ec4
Daniel Loyd <dhloyd@icloud.com>
2020-02-02 10:54:00 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -25,6 +25,10 @@ def monopoly_with_fourth_tier
 	  :railroads => {
 	    pieces: 4
 	    rent_in_dollars: {
+	      one_piece_owned: 25,
+	      two_pieces_owned: 50, 
+	      three_pieces_owned:
+	      four_pieces_owned:
 	      
 	    }
 	  }
